dc74a22c44c4be081bec8a4b3946ca6ed20864dc
DashaKalina <darina.perevyazko@gmail.com>
2021-12-28 16:08:47 +0200

    using cycle types

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// // цикл «for» от 1 до 8, с шагом 1
+// for (var i = 1; i <= 8; i++) {
+//   console.log(i);
+// }
+
+let i=1;
+while (i<=8) {
+  console.log(i);
+  i++;
+}
+
+// let i=1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i<=8);
+
+
+
+
